90931a09d538bc943913eca9e70197b2df128744
parkjisu0107 <parkjisu0107@gmail.com>
2023-11-23 16:37:37 +0900

    231123

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { MdDelete, MdDone } from 'react-icons/md';
+import cn from 'classnames';
+
 import './scss/TodoItem.scss';
 
-const TodoItem = ({ item }) => {
+const TodoItem = ({ item, remove, check }) => {
+  const { id, title, done } = item;
+
   return (
     <li className='todo-list-item'>
-      <div className='check-circle'>
+      <div
+        className={cn('check-circle', { active: done })}
+        onClick={() => check(id)}
+      >
         <MdDone />
       </div>
-      <span className='text'>{item.title}</span>
-      <div className='remove'>
+      <span className={cn('text', { finish: done })}>{title}</span>
+      <div
+        className='remove'
+        onClick={() => remove(id)}
+      >
         <MdDelete />
       </div>
     </li>
--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -31,7 +31,7 @@ const TodoTemplate = () => {
     },
   ]);
 
-  // id값 시퀀스 함수 (DB 연동시키면 필요 없음!)
+  // id값 시퀀스 함수 (DB 연동시키면 필요없게 됨.)
   const makeNewId = () => {
     if (todos.length === 0) return 1;
     return todos[todos.length - 1].id + 1; // 맨 마지막 할일 객체의 id보다 하나 크게
@@ -39,7 +39,7 @@ const TodoTemplate = () => {
 
   /*
     todoInput에게 todoText를 받아오는 함수
-    자식 컴포넌트가 부모컴포넌트에게 데이터를 전달할 때는
+    자식 컴포넌트가 부모컴포넌트에게 데이터를 전달할 때는 
     일반적인 props 사용이 불가능.
     부모 컴포넌트에서 함수를 선언(매개변수 꼭 선언) -> props로 함수를 전달
     자식 컴포넌트에서 전달받은 함수를 호출하면서 매개값으로 데이터를 전달.
@@ -61,10 +61,32 @@ const TodoTemplate = () => {
     });
   };
 
+  // 할 일 삭제 처리 함수
+  const removeTodo = (id) => {
+    // 주어진 배열의 값들을 순회하여 조건에 맞는 요소들만 모아서 새로운 배열로 리턴.
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  // 할 일 체크 처리 함수
+  const checkTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+
+  // 체크가 안 된 할 일의 개수 카운트 하기
+  const countRestTodo = () => todos.filter((todo) => !todo.done).length;
+
   return (
     <div className='TodoTemplate'>
-      <TodoHeader />
-      <TodoMain todoList={todos} />
+      <TodoHeader count={countRestTodo} />
+      <TodoMain
+        todoList={todos}
+        remove={removeTodo}
+        check={checkTodo}
+      />
       <TodoInput addTodo={addTodo} />
     </div>
   );
